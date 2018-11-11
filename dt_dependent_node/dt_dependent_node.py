a62053444f95ac87f32eca5ea3f47cf7ec310ad7
bramtoula <benjaminramtoula@gmail.com>
2018-11-11 01:23:17 -0500

    experiment with adding a node

--- dt_dependent_node/dt_dependent_node.py
+++ dt_dependent_node/dt_dependent_node.py
@@ -6,14 +6,14 @@ import numpy as np
 from duckietown_msgs.msg import WheelsCmdStamped
 
 def continuous_publisher():
-    vel_pub = rospy.Publisher('/random/topic/', WheelsCmdStamped, queue_size=1)
+    vel_pub = rospy.Publisher('/default/wheels_driver_node/wheels_cmd', WheelsCmdStamped, queue_size=1)
     rospy.init_node('continuous_test_publisher')
     rate = rospy.Rate(10)
-    
+
     while not rospy.is_shutdown():
         msg = WheelsCmdStamped()
-        msg.vel_left = np.random.random()
-        msg.vel_right = np.random.random()
+        msg.vel_left = 1.0
+        msg.vel_right = 1.0
 
         vel_pub.publish(msg)
         rate.sleep()
@@ -21,5 +21,3 @@ def continuous_publisher():
 
 if __name__ == '__main__':
     continuous_publisher()
-
-
